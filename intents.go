da3e1424372f6831d28d44bb0b127ad3a148b56c
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2023-11-29 00:34:25 +0200

    Add validation and unit tests

--- intents.go
+++ intents.go
@@ -1,5 +1,13 @@
 package intents
 
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+)
+
 type Kind string
 type Address string
 
@@ -10,14 +18,53 @@ const (
 
 type Intent struct {
 	Kind              `json:"kind" binding:"required"`
-	SellToken         string  `json:"sellToken" binding:"required"`
-	BuyToken          string  `json:"buyToken" binding:"required"`
-	SellAmount        float64 `json:"sellAmount" binding:"required"`
-	BuyAmount         float64 `json:"buyAmount" binding:"required"`
+	SellToken         string  `json:"sellToken" binding:"required,token_name"`
+	BuyToken          string  `json:"buyToken" binding:"required,token_name"`
+	SellAmount        float64 `json:"sellAmount" binding:"required,float"`
+	BuyAmount         float64 `json:"buyAmount" binding:"required,float"`
 	PartiallyFillable bool    `json:"partiallyFillable" `
 }
 
 type Body struct {
 	Sender  Address  `json:"sender" binding:"required,eth_addr"`
 	Intents []Intent `json:"intents" binding:"required,dive"`
+}
+
+func validEthAddress(fl validator.FieldLevel) bool {
+	addressHex := fl.Field().String()
+	return common.IsHexAddress(addressHex)
+}
+
+func validKind(fl validator.FieldLevel) bool {
+	kind := fl.Field().String()
+	return kind == string(Buy) || kind == Sell
+}
+
+func validTokenName(fl validator.FieldLevel) bool {
+	tokenName := fl.Field().String()
+	return tokenName != ""
+}
+
+func validFloat(fl validator.FieldLevel) bool {
+	float := fl.Field().String()
+	return float != ""
+}
+
+func NewValidator() error {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		if err := v.RegisterValidation("eth_addr", validEthAddress); err != nil {
+			return fmt.Errorf("validator %s failed", "eth_addr")
+		}
+		if err := v.RegisterValidation("kind", validKind); err != nil {
+			return fmt.Errorf("validator %s failed", "kind")
+		}
+		if err := v.RegisterValidation("token_name", validTokenName); err != nil {
+			return fmt.Errorf("validator %s failed", "token_name")
+		}
+		if err := v.RegisterValidation("float", validFloat); err != nil {
+			return fmt.Errorf("validator %s failed", "float")
+		}
+	}
+
+	return nil
 }
---
+++ intents_test.go
@@ -0,0 +1,84 @@
+package intents
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func submitHandler(c *gin.Context) {
+	var body Body
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// Process the valid request
+	c.JSON(http.StatusOK, gin.H{"message": "Received successfully"})
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	if err := NewValidator(); err != nil {
+		panic(err)
+	}
+
+	r.POST("/submit", submitHandler)
+	return r
+}
+
+func TestSubmitHandler(t *testing.T) {
+	// Setup
+	router := setupRouter()
+
+	// Define test cases
+	testCases := []struct {
+		description string
+		payload     Body
+		expectCode  int
+	}{
+		{
+			description: "Valid Request",
+			payload: Body{
+				Sender: "0x0A7199a96fdf0252E09F76545c1eF2be3692F46b",
+				Intents: []Intent{
+					{
+						Kind:       "buy",
+						SellToken:  "TokenA",
+						BuyToken:   "TokenB",
+						SellAmount: 10.0,
+						BuyAmount:  5.0,
+					},
+				},
+			},
+			expectCode: http.StatusOK,
+		},
+		{
+			description: "Invalid Request (missing fields)",
+			payload: Body{
+				Sender: "0xValidEthAddress",
+			},
+			expectCode: http.StatusBadRequest,
+		},
+	}
+
+	// Run test cases
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			payloadBytes, _ := json.Marshal(tc.payload)
+			req, _ := http.NewRequest("POST", "/submit", bytes.NewBuffer(payloadBytes))
+			req.Header.Set("Content-Type", "application/json")
+
+			w := httptest.NewRecorder()
+			router.ServeHTTP(w, req)
+
+			if w.Code != tc.expectCode {
+				t.Errorf("Expected status code %d, got %d", tc.expectCode, w.Code)
+			}
+		})
+	}
+}
