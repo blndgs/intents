f99aa07ea779ce34704303a36505da3f8c9cf594
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2023-11-29 17:02:09 +0200

    feat: Add new fields and validators

--- intents.go
+++ intents.go
@@ -8,25 +8,44 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-type Kind string
 type Address string
+
+type Kind string
 
 const (
 	Buy  Kind = "buy"
 	Sell      = "sell"
 )
 
+type ProcessingStatus string
+
+const (
+	Received     ProcessingStatus = "Received"
+	SentToSolver ProcessingStatus = "SentToSolver"
+	Solved       ProcessingStatus = "Solved"
+	Expired      ProcessingStatus = "Expired"
+	OnChain      ProcessingStatus = "OnChain"
+	Invalid      ProcessingStatus = "Invalid"
+	// evaluate adding specific on chain statuses
+)
+
 type Intent struct {
-	Kind              `json:"kind" binding:"required"`
-	SellToken         string  `json:"sellToken" binding:"required,token_name"`
-	BuyToken          string  `json:"buyToken" binding:"required,token_name"`
-	SellAmount        float64 `json:"sellAmount" binding:"required,float"`
-	BuyAmount         float64 `json:"buyAmount" binding:"required,float"`
-	PartiallyFillable bool    `json:"partiallyFillable" `
+	Sender            Address          `json:"sender" binding:"required,eth_addr"`      // ui
+	Kind              Kind             `json:"kind" binding:"required"`                 // ui
+	Hash              string           `json:"hash binding:"required"`                  // ui or bundler
+	SellToken         string           `json:"sellToken" binding:"required,token_name"` // ui
+	BuyToken          string           `json:"buyToken" binding:"required,token_name"`  // ui
+	SellAmount        float64          `json:"sellAmount" binding:"required,float"`     // ui
+	BuyAmount         float64          `json:"buyAmount" binding:"required,float"`      // ui
+	PartiallyFillable bool             `json:"partiallyFillable"`                       // ui
+	CallData          string           `json:"callData"`                                // Solver
+	Status            ProcessingStatus `json:"status" binding:"required,status"`        // ui or bundler
+	CreatedAt         uint64           `json:"createdAt"`                               // ui
+	ExpirationAt      uint64           `json:"expirationAt"`                            // ui or bundler for default expiration
 }
 
 type Body struct {
-	Sender  Address  `json:"sender" binding:"required,eth_addr"`
+	Sender  Address  `json:"sender" binding:"required,eth_addr,valid_senders"`
 	Intents []Intent `json:"intents" binding:"required,dive"`
 }
 
@@ -50,8 +69,42 @@ func validFloat(fl validator.FieldLevel) bool {
 	return float != ""
 }
 
+func validSenders(fl validator.FieldLevel) bool {
+	body, ok := fl.Top().Interface().(Body)
+	if !ok {
+		return false
+	}
+
+	// Check if the Sender in the Body is non-empty
+	if body.Sender == "" {
+		return false
+	}
+
+	senderAddress := body.Sender
+
+	// Check if any Sender in the Intents is non-empty
+	for _, intent := range body.Intents {
+		if intent.Sender != senderAddress {
+			return false
+		}
+	}
+
+	return true
+}
+
+func validStatus(fl validator.FieldLevel) bool {
+	status := fl.Field().String()
+	return status == string(Received) || status == string(SentToSolver) || status == string(Solved) || status == string(Expired) || status == string(OnChain) || status == string(Invalid)
+}
+
 func NewValidator() error {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		if err := v.RegisterValidation("status", validStatus); err != nil {
+			return fmt.Errorf("validator %s failed", "status")
+		}
+		if err := v.RegisterValidation("valid_senders", validSenders); err != nil {
+			return fmt.Errorf("validator %s failed", "valid_senders")
+		}
 		if err := v.RegisterValidation("eth_addr", validEthAddress); err != nil {
 			return fmt.Errorf("validator %s failed", "eth_addr")
 		}
