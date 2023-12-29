5f7b4979b22d659cba1b69652acf21ccd501b208
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2023-12-30 00:13:10 +0200

    feat: Add String() unit test

--- userops_ext_test.go
+++ userops_ext_test.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/big"
 	"reflect"
 	"testing"
@@ -159,5 +160,58 @@ func TestUserOperation_UnmarshalJSON(t *testing.T) {
 	// Compare the original and unmarshalled instances
 	if !reflect.DeepEqual(originalOp, &unmarshalledOp) {
 		t.Errorf("Unmarshalled UserOperation does not match the original.\nOriginal: %+v\nUnmarshalled: %+v", originalOp, unmarshalledOp)
+	}
+}
+
+func TestUserOperationString(t *testing.T) {
+	userOp := UserOperation{
+		Sender:               common.HexToAddress("0x6B5f6558CB8B3C8Fec2DA0B1edA9b9d5C064ca47"),
+		Nonce:                big.NewInt(0x7),
+		InitCode:             []byte{},
+		CallData:             []byte{},
+		CallGasLimit:         big.NewInt(0x2dc6c0),
+		VerificationGasLimit: big.NewInt(0x2dc6c0),
+		PreVerificationGas:   big.NewInt(0xbb70),
+		MaxFeePerGas:         big.NewInt(0x7e498f31e),
+		MaxPriorityFeePerGas: big.NewInt(0x7e498f300),
+		PaymasterAndData:     []byte{},
+		Signature:            []byte{0xbd, 0xa2, 0x86, 0x5b, 0x91, 0xc9, 0x2e, 0xf7, 0xf8, 0xa4, 0x3a, 0xdc, 0x03, 0x9b, 0x8a, 0x3f, 0x43, 0x01, 0x1a, 0x20, 0xcf, 0xc8, 0x18, 0xd0, 0x78, 0x84, 0x7e, 0xf2, 0xff, 0xd9, 0x16, 0xec, 0x23, 0x6a, 0x1c, 0xc9, 0x21, 0x8b, 0x16, 0x4f, 0xe2, 0xf5, 0xa7, 0x08, 0x8b, 0x70, 0x10, 0xc9, 0x0a, 0xd0, 0xf9, 0xa9, 0xdc, 0xf3, 0xa2, 0x11, 0x68, 0xd4, 0x33, 0xe7, 0x84, 0x58, 0x2a, 0xfb, 0x1c},
+	}
+
+	// Define the expected string with new formatting.
+	expected := fmt.Sprintf(
+		`UserOperation{
+  Sender: %s
+  Nonce: %s
+  InitCode: %s
+  CallData: %s
+  CallGasLimit: %s
+  VerificationGasLimit: %s
+  PreVerificationGas: %s
+  MaxFeePerGas: %s
+  MaxPriorityFeePerGas: %s
+  PaymasterAndData: %s
+  Signature: %s
+}`,
+		userOp.Sender.String(),
+		"0x7, 7",
+		"0x",
+		"0x",
+		"0x2dc6c0, 3000000",
+		"0x2dc6c0, 3000000",
+		"0xbb70, 47984",
+		"0x7e498f31e, 33900000030",
+		"0x7e498f300, 33900000000",
+		"0x",
+		"0xbda2865b91c92ef7f8a43adc039b8a3f43011a20cfc818d078847ef2ffd916ec236a1cc9218b164fe2f5a7088b7010c90ad0f9a9dcf3a21168d433e784582afb1c", // Signature as hex
+	)
+
+	// Call the String method.
+	result := userOp.String()
+	t.Log(result)
+
+	// Compare the result with the expected string.
+	if result != expected {
+		t.Errorf("String() = %v, want %v", result, expected)
 	}
 }
